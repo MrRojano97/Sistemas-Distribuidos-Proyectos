0b8420bebcc1aa5e4dee5a562e0407b2560e61d3
Elías Avendaño Núñez <mrrojano97@icloud.com>
2020-10-20 01:00:02 -0300

    Creación de clase PGMFile
    - Fallos Conocidos: No se puede guardar una imagen como P5, sólo como P2
    - Esta clase permite crear un archivo PGM entregándole los datos necesarios
    - Puedes importar un archivo PGM hacia él, ya sea P5 o P2, siempre lo almacenará en memoria como P2
    - Puedes exportar un archivo PGM (P2)
    - Puedes transformar de P5 a P2 los PGM

--- Proyecto 1/src/Main.java
+++ Proyecto 1/src/Main.java
@@ -1,6 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        PGMFile file1 = new PGMFile("Macaco");
+        //file1.print();
+        file1.load(new File(args[0]));
+        file1.save("P2");
+        file1.save("P5");
     }
 
 }
---
+++ Proyecto 1/src/PGMFile.java
@@ -0,0 +1,226 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PGMFile {
+    private String magicNumber;
+    private String comment="";
+    private BufferedReader fileBuffered;
+    private String line;
+
+    private Scanner ssize;
+    private String size;
+    private int sizeW;
+    private int sizeH;
+    private int maxGrayValue;
+
+    private Scanner actualLine;
+
+    private AtomicInteger[][] image;
+    private File file;
+
+    private String name;
+
+
+    public PGMFile(String name) throws IOException {
+        this.name=name;
+    }
+
+    public void load(File file) throws IOException {
+        fileBuffered= new BufferedReader(new FileReader(file));
+        magicNumber = fileBuffered.readLine();    //Primera línea siempre llevará el número mágico
+        line = fileBuffered.readLine(); //Segunda línea puede llevar comentario o tamaño
+
+        if (line.startsWith("#")) { //Si la segunda línea comienza con un "#" se entiende que es un comentario
+            comment = line;
+            size = fileBuffered.readLine(); //String que contiene los dos tamaños
+        }
+
+        else { //Si ese no es el caso, entonces se trataba del String de los tamaños
+            size = line;
+        }
+
+        ssize = new Scanner(size);
+
+        sizeW = ssize.nextInt();
+        sizeH = ssize.nextInt();
+
+        maxGrayValue = Integer.parseInt(fileBuffered.readLine());
+
+        image = new AtomicInteger[sizeH][sizeW];
+        byte[][] im = new byte[sizeH][sizeW];
+
+        int count = 0;
+        int b = 0;
+
+        if (magicNumber.equals("P5")){
+            FileInputStream fileInputStream = new FileInputStream(file);
+            DataInputStream dis = new DataInputStream(fileInputStream);
+
+            int numnewlines = 0;
+
+            if (comment.equals(""))
+                numnewlines = 3;
+            else
+                numnewlines = 4;
+
+            while (numnewlines > 0) {
+                char c;
+                do {
+                    c = (char)(dis.readUnsignedByte());
+                } while (c != '\n');
+                numnewlines--;
+            }
+
+            // read the image data
+
+            for (int i = 0; i < sizeH; i++) {
+                for (int j = 0; j < sizeW; j++) {
+                    image[i][j] = new AtomicInteger(dis.readUnsignedByte());
+                }
+            }
+        }
+
+        if (magicNumber.equals("P2")){
+            actualLine = new Scanner(fileBuffered.readLine());
+            for (int i = 0; i<sizeH; i++) {
+                for (int j = 0; j<sizeW; j++) {
+                    if (!actualLine.hasNext()){
+                        actualLine = new Scanner(fileBuffered.readLine());
+                    }
+                    image[i][j] = new AtomicInteger(actualLine.nextInt());
+                }
+            }
+        }
+    }
+
+    public void print() {
+
+        //Se imprime la cabecera
+        System.out.println(magicNumber);
+        if (comment!=""){
+            System.out.println(comment);
+        }
+        System.out.println(size);
+        System.out.println(maxGrayValue);
+
+        //Se imprimen los valores
+        for (int i=0;i<sizeH;i++){
+            for(int j=0;j<sizeW;j++){
+                System.out.print(image[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public void save(String magicNumber) {
+
+        if (magicNumber.equals("P2")) {
+            try {
+                FileWriter fstream = new FileWriter(name + " outP2.pgm");
+                BufferedWriter out = new BufferedWriter(fstream);
+
+                //Se escribe la cabecera
+                if (comment != "") {
+                    out.write(magicNumber + "\n" + comment + "\n" + size + "\n" + maxGrayValue + "\n");
+                } else {
+                    out.write(magicNumber + "\n" + size + "\n" + maxGrayValue + "\n");
+                }
+
+                //Se escriben los valores
+                for (int i = 0; i < sizeH; i++) {
+                    for (int j = 0; j < sizeW; j++) {
+                        out.write(Integer.toString(image[i][j].get()));
+                        if (j != sizeW - 1)
+                            out.write(" ");
+                    }
+                    if (i != sizeH - 1)
+                        out.write("\n");
+                }
+                out.close();
+            } catch (Exception e) {
+                System.err.println("Error : " + e.getMessage());
+            }
+
+        }
+        if (magicNumber.equals("P5")) {
+            try {
+                FileWriter fstream = new FileWriter(name + " outP5.pgm");
+                BufferedWriter out = new BufferedWriter(fstream);
+
+                //Se escribe la cabecera
+                if (comment != "") {
+                    out.write(magicNumber + "\n" + comment + "\n" + size + "\n" + maxGrayValue + "\n");
+                } else {
+                    out.write(magicNumber + "\n" + size + "\n" + maxGrayValue + "\n");
+                }
+
+                //Se escriben los valores
+                int count = 0;
+                for (int i = 0; i < sizeH; i++) {
+                    for (int j = 0; j < sizeW; j++) {
+                        //?????
+
+                        if (j!= sizeW - 1)
+                            out.write(" ");
+                    }
+                    if (i!= sizeH - 1)
+                        out.write("\n");
+
+                }
+                out.close();
+            } catch (Exception e) {
+                System.err.println("Error : " + e.getMessage());
+            }
+        }
+    }
+
+    public String getMagicNumber() {
+        return magicNumber;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public int getSizeW() {
+        return sizeW;
+    }
+
+    public int getSizeH() {
+        return sizeH;
+    }
+
+    public int getMaxGrayValue() {
+        return maxGrayValue;
+    }
+
+    public AtomicInteger[][] getImage() {
+        return image;
+    }
+
+    public void setMagicNumber(String magicNumber) {
+        this.magicNumber = magicNumber;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setSizeW(int sizeW) {
+        this.sizeW = sizeW;
+    }
+
+    public void setSizeH(int sizeH) {
+        this.sizeH = sizeH;
+    }
+
+    public void setMaxGrayValue(int maxGrayValue) {
+        this.maxGrayValue = maxGrayValue;
+    }
+
+    public void setImage(AtomicInteger[][] image) {
+        this.image = image;
+    }
+}
